bf14f6889e38ee8b9d3e60e63c2321371b098d12
Tim John <chezzyt13@gmail.com>
2020-04-29 11:18:52 +0100

    Access Modifiers & Constructors

--- src/com/assignment/Car.java
+++ src/com/assignment/Car.java
@@ -1,14 +1,49 @@
 package com.assignment;
 
 public class Car {
+    Car(){
+        this.setColor("Black");
+        this.setWheels(4);
+    }
 
-    public String color;
+    Car(int wheels){
+        this.setColor("Black");
+        this.setWheels(wheels);
+    }
 
-    public int wheels;
+    Car(String color){
+        this.setColor(color);
+        this.setWheels(4);
+    }
+
+    Car(String color, int wheels){
+        this.setColor(color);
+        this.setWheels(wheels);
+    }
+
+    private String color;
+
+    private int wheels;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
 
     public void printDetails(){
-        System.out.println("Car wheels:" + this.wheels);
-        System.out.println("Car colour:" + this.color);
+        System.out.println("Car wheels: " + this.wheels);
+        System.out.println("Car colour: " + this.color);
     }
 
 }
--- src/com/assignment/Hello.java
+++ src/com/assignment/Hello.java
@@ -3,11 +3,8 @@ package com.assignment;
 public class Hello {
 
     public static void main(String[] args) {
-
         Car car = new Car();
 
-
         car.printDetails();
-
     }
 }
