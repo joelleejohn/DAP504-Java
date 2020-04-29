a36b0ea9917ad0984e0f4be16bc3a48c1aeb24fd
Tim John <chezzyt13@gmail.com>
2020-04-29 12:04:50 +0100

    Array without explict length using List<String>

--- src/com/assignment/Car.java
+++ src/com/assignment/Car.java
@@ -1,14 +1,23 @@
 package com.assignment;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Car {
     Car(){
         this.setColor("Black");
         this.setWheels(4);
+        this.previousOwners.add("Roger");
     }
 
-    Car(int wheels){
+    Car(int wheels, String... previousOwners){
         this.setColor("Black");
         this.setWheels(wheels);
+
+        for (String prevOwner: previousOwners){
+            this.previousOwners.add(prevOwner);
+        }
     }
 
     Car(String color){
@@ -21,6 +30,8 @@ public class Car {
         this.setWheels(wheels);
     }
 
+    private List<String> previousOwners = new ArrayList<String>();
+    
     private String color;
 
     private int wheels;
@@ -44,6 +55,10 @@ public class Car {
     public void printDetails(){
         System.out.println("Car wheels: " + this.wheels);
         System.out.println("Car colour: " + this.color);
+
+        for (int position = 0;  position < this.previousOwners.size(); position++) {
+            System.out.println("Owner number " + position + " is " + this.previousOwners.get(position) );
+        }
     }
 
 }
--- src/com/assignment/Hello.java
+++ src/com/assignment/Hello.java
@@ -1,10 +1,18 @@
 package com.assignment;
 
+import java.util.List;
+
 public class Hello {
 
     public static void main(String[] args) {
+        List<String> prevOwners;
+
         Car car = new Car();
 
         car.printDetails();
+
+        Car car2 = new Car(6, "Tim", "James", "Roger");
+
+        car2.printDetails();
     }
 }
