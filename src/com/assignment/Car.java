eaa228cb2ce4346b11e1600a992639d37fd52810
Tim John <chezzyt13@gmail.com>
2020-04-29 10:13:04 +0100

    Initial commit

---
+++ src/com/assignment/Car.java
@@ -0,0 +1,14 @@
+package com.assignment;
+
+public class Car {
+
+    public String color;
+
+    public int wheels;
+
+    public void printDetails(){
+        System.out.println("Car wheels:" + this.wheels);
+        System.out.println("Car colour:" + this.color);
+    }
+
+}
---
+++ src/com/assignment/Hello.java
@@ -0,0 +1,13 @@
+package com.assignment;
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        Car car = new Car();
+
+
+        car.printDetails();
+
+    }
+}
